8f74c76a01e57659363c95c9da4b293408ca7fde
Rohit Goswami <rgoswami@ieee.org>
2026-03-08 09:13:57 +0100

    git subrepo clone --branch=feat/chemgp-theme-export git@github.com:HaoZeke/chemparseplot.git external/chemparseplot
    subrepo:
      subdir:   "external/chemparseplot"
      merged:   "15993ad"
    upstream:
      origin:   "git@github.com:HaoZeke/chemparseplot.git"
      branch:   "feat/chemgp-theme-export"
      commit:   "15993ad"
    git-subrepo:
      version:  "0.4.9"
      origin:   "???"
      commit:   "???"

---
+++ external/chemparseplot/chemparseplot/units.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2023-present Rohit Goswami <rog32@hi.is>
+#
+# SPDX-License-Identifier: MIT
+"""Unit registry and quantity helpers via pint.
+
+```{versionadded} 0.0.2
+```
+"""
+
+import warnings
+
+import pint
+
+ureg = pint.UnitRegistry(cache_folder=":auto:")
+ureg.define("kcal_mol = kcal / 6.02214076e+23 = kcm")
+Q_ = ureg.Quantity
+
+# Silence NEP 18 warning
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    Q_([])
